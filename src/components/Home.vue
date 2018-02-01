26617f335072547f57dc5e609c8f94cd08e47822
rich36@gmail.com <rreuter@cingroup.com>
2018-02-01 16:18:13 -0500

    Adding in some dummy elements

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,19 +3,41 @@
         <div>
             <h3>Upcoming Performances Near You</h3>
 
+            
+                <div v-for="performance in performances">
+                    <h2>{{performance.venue.name}}</h2>
+                    <p>{{performance.dates.start}}</p>
+                </div>
+            
         </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Home',
   data () {
     return {
       'performances' : [
           {
               'venue' : {
-                  'name': 'Yellow Cab Tavern'
+                  'name': 'Yellow Cab Tavern',
+                  'address' : {
+                      'street': '',
+                      'street 2' : '',
+                      'city': 'Dayton',
+                      'state' : 'OH',
+                      'zip' : '45402'
+                  },
+                  'website' : '',
+                  'phone' : ''
+              },
+              'organizer' :{
+                  'name' : 'Mr. Open Mic'
+              },
+              'dates': {
+                  'start' : '2018-01-02 8:00 PM'
               }
+
           }
       ]
     }
