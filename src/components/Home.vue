a84b389e03396acec7783a502a69867b15d434ea
Rich Reuter <rreuter@cingroup.com>
2018-02-01 15:53:13 -0500

    Adding in basic boostrap template and starting to build out the routing and components

---
+++ src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+
+        <div>
+            <h3>Upcoming Performances Near You</h3>
+
+        </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      'performances' : [
+          {
+              'venue' : {
+                  'name': 'Yellow Cab Tavern'
+              }
+          }
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
